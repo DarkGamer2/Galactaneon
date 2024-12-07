91d954e47728471060b4bfa21b5b70b04bb250b3
Kameer Hosein <kameer-hosien@outlook.com>
2024-12-07 13:12:37 -0400

    added images to carousel

--- galactaneon/src/components/ParallaxCarousel.tsx
+++ galactaneon/src/components/ParallaxCarousel.tsx
@@ -1,13 +1,15 @@
-import React, { useRef, useState } from "react";
+import React from "react";
 // Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
-
+import GirlCosplay from "../assets/media/images/Miku-Fashion-Subculture-Dress-Uniform-Outfit-Anime-Cosplay-Costumes.jpg_640x640Q90.jpg_.avif"
+import ValorantCosplay from "../assets/media/images/fade-cosplay-v0-cm83kyh26fra1.webp"
 // Import Swiper styles
 import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
 
 import { Parallax, Pagination, Navigation } from "swiper";
+
 const ParallaxCarousel = () => {
   return (
     <>
@@ -15,7 +17,7 @@ const ParallaxCarousel = () => {
         style={{
           "--swiper-navigation-color": "#fff",
           "--swiper-pagination-color": "#fff",
-        }}
+        } as React.CSSProperties}
         speed={600}
         parallax={true}
         pagination={{
@@ -36,40 +38,20 @@ const ParallaxCarousel = () => {
         ></div>
         <SwiperSlide>
           <div className="title" data-swiper-parallax="-300">
-            Slide 1
+            <h3 className="dark:text-white text-center">Girl Cosplay</h3>
           </div>
-          <div className="subtitle" data-swiper-parallax="-200">
-            Subtitle
-          </div>
+          <div className="subtitle" data-swiper-parallax="-200"></div>
           <div className="text" data-swiper-parallax="-100">
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
-              dictum mattis velit, sit amet faucibus felis iaculis nec. Nulla
-              laoreet justo vitae porttitor porttitor. Suspendisse in sem justo.
-              Integer laoreet magna nec elit suscipit, ac laoreet nibh euismod.
-              Aliquam hendrerit lorem at elit facilisis rutrum. Ut at
-              ullamcorper velit. Nulla ligula nisi, imperdiet ut lacinia nec,
-              tincidunt ut libero. Aenean feugiat non eros quis feugiat.
-            </p>
+            <img src={GirlCosplay} alt="Girl Cosplay" className="carousel-image" />
           </div>
         </SwiperSlide>
         <SwiperSlide>
           <div className="title" data-swiper-parallax="-300">
-            Slide 2
+            <h3 className="dark:text-white text-center">Valorant Cosplay</h3>
           </div>
-          <div className="subtitle" data-swiper-parallax="-200">
-            Subtitle
-          </div>
+          <div className="subtitle" data-swiper-parallax="-200"></div>
           <div className="text" data-swiper-parallax="-100">
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
-              dictum mattis velit, sit amet faucibus felis iaculis nec. Nulla
-              laoreet justo vitae porttitor porttitor. Suspendisse in sem justo.
-              Integer laoreet magna nec elit suscipit, ac laoreet nibh euismod.
-              Aliquam hendrerit lorem at elit facilisis rutrum. Ut at
-              ullamcorper velit. Nulla ligula nisi, imperdiet ut lacinia nec,
-              tincidunt ut libero. Aenean feugiat non eros quis feugiat.
-            </p>
+            <img src={ValorantCosplay} alt="Valorant Cosplay" className="carousel-image" />
           </div>
         </SwiperSlide>
         <SwiperSlide>
@@ -84,10 +66,6 @@ const ParallaxCarousel = () => {
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
               dictum mattis velit, sit amet faucibus felis iaculis nec. Nulla
               laoreet justo vitae porttitor porttitor. Suspendisse in sem justo.
-              Integer laoreet magna nec elit suscipit, ac laoreet nibh euismod.
-              Aliquam hendrerit lorem at elit facilisis rutrum. Ut at
-              ullamcorper velit. Nulla ligula nisi, imperdiet ut lacinia nec,
-              tincidunt ut libero. Aenean feugiat non eros quis feugiat.
             </p>
           </div>
         </SwiperSlide>
